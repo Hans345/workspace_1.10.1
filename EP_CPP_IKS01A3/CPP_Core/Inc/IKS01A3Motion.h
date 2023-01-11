bca519f468c2743122b26b2ed8e7fc6e45e2f164
Raphael Baumeler <raphael.baumeler@ost.ch>
2023-01-11 14:46:45 +0100

    Done: Exercise 3.1-3.3

--- EP_CPP_IKS01A3/CPP_Core/Inc/IKS01A3Motion.h
+++ EP_CPP_IKS01A3/CPP_Core/Inc/IKS01A3Motion.h
@@ -9,6 +9,9 @@
 #define INC_IKS01A3MOTION_H_
 
 #include <array>
+#include <stdio.h>		//For printf()
+#include <algorithm>	//For std::rotate()
+#include <numeric>		//For std::accumulate()
 #include "iks01a3_motion_sensors.h"
 
 #define BUFFERSIZE 6
@@ -17,12 +20,9 @@ class IKS01A3_Motion {
 	private:
 		IKS01A3_MOTION_SENSOR_Axes_t AxisValues;
 		IKS01A3_MOTION_SENSOR_Axes_t AxisOffsets;
-		std::array<int32_t, BUFFERSIZE> RingBufferAxisX;
-		std::array<int32_t, BUFFERSIZE> RingBufferAxisY;
-		std::array<int32_t, BUFFERSIZE> RingBufferAxisZ;
-	    uint8_t WriteIndexRingBufferAxisX=0;
-	    uint8_t WriteIndexRingBufferAxisY=0;
-	    uint8_t WriteIndexRingBufferAxisZ=0;
+		std::array<int32_t, BUFFERSIZE> RingBufferAxisX={0,0,0,0,0,0};
+		std::array<int32_t, BUFFERSIZE> RingBufferAxisY={0,0,0,0,0,0};
+		std::array<int32_t, BUFFERSIZE> RingBufferAxisZ={0,0,0,0,0,0};
 	public:
 		IKS01A3_Motion();
 		virtual ~IKS01A3_Motion();
